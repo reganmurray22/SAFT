72559a5878544a3a3ec12ff174a35aa1f538aff1
Nina Koller <nckoller@gmail.com>
2020-08-12 21:08:08 -0500

    pseudo code plan

---
+++ script.js
@@ -0,0 +1,29 @@
+// Search Bar takes in value, click event w/ button
+
+// Get Value from search bar, pass to API call=>
+
+// API call to get movie data
+
+// Parse movie data to display:
+
+// Poster
+
+// Year
+
+// Genre
+
+// Director
+
+// Actors
+
+// Select Search Criteria w/ check boxes (one item to start)
+
+// Make 1 or 2 API calls to suggest 3 movies to watch
+
+// Stretch goal: When you click one of the recommended movies
+// display additional information
+
+// If you want 3 different movies prompted, what happens?
+
+//Stretch Item: A button to suggest a movie if you
+// can't think of one to watch
